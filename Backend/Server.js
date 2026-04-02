d10575014dadcbae7c971b1ffa944126c57457de
sonamanayakkar <esonaman2@gmail.com>
2026-04-02 10:12:45 +0530

    backend server created withb node js5

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,31 +1,31 @@
 import express from "express";
 import axios from "axios";
 import cors from "cors";
-
+const PORT = process.env.PORT || 5000
 const app = express();
 app.use(cors());
 
 app.get("/flights", async (req, res) => {
-  try {
-    const response = await axios.get(
-      "https://opensky-network.org/api/states/all",
-      {
-        auth: {
-          username: "sonamanayakkar-api-client",
-          password: "zXIpuzwZvxb9GNEyVfJVDZA8gftSfdRl"
-        }
-      }
-    );
+    try {
+        const response = await axios.get(
+            "https://opensky-network.org/api/states/all",
+            {
+                auth: {
+                    username: "sonamanayakkar-api-client",
+                    password: "zXIpuzwZvxb9GNEyVfJVDZA8gftSfdRl"
+                }
+            }
+        );
 
-    res.json({
-      data: response.data.states
-    });
+        res.json({
+            data: response.data.states
+        });
 
-  } catch (err) {
-    res.status(500).json({ error: "Failed" });
-  }
+    } catch (err) {
+        res.status(500).json({ error: "Failed" });
+    }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+app.listen(PORT, () => {
+    console.log("Server running on http://localhost:5000");
 });
